d0f99101ab9ed2e687a4517eb1e5e5b605efdee8
Eza Angga Alfian <eza.anggaa@gmail.com>
2023-07-30 17:24:03 +0700

    Integration Landing Page, Checkout, and Dashboard

---
+++ app/Http/Controllers/User/CheckoutController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Camp;
+use App\Models\Checkout;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Camp $camp)
+    {
+        return view('checkout.create', [
+            'camp' => $camp
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Camp $camp)
+    {
+
+        // Mapping request data
+        $data = $request->all();
+        $data['user_id'] = Auth::id();
+        $data['camp_id'] = $camp->id;
+
+        // Update data user
+        $user = Auth::user();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->occupation = $data['occupation'];
+        $user->save();
+
+        $checkout = Checkout::create($data);
+
+        // return $checkout;
+        return redirect(route('checkout.success'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Checkout $checkout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Checkout $checkout)
+    {
+        //
+    }
+
+    public function success()
+    {
+        return view('checkout.success');
+    }
+}
